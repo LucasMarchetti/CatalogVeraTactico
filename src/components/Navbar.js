47c84ad80ab95c449cbc24f67918feba0c7f56e2
LucasMarchetti <luksmarchetti@gmail.com>
2024-05-28 12:37:38 -0300

    Fix responsive navbar and add hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 
+// React
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 // Styles
 import "./Navbar.css";
 import LogoMarca from "./images/logo.png";
 
 const NavBar = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    const closeMenu = () => setClick(false);
+
     return (
         <div className="header">
             <nav className="navbar"> 
@@ -11,24 +21,29 @@ const NavBar = () => {
                     <img src={LogoMarca} alt="Img Logo" />
                     <h1 style={{ color: "black" }}>Vera Táctico</h1>
                 </a>
-                <ul className="nav-menu" >
+                {click ? (
+                    <FaTimes style={{ color: "black" }} className="hamburger" onClick={handleClick} />
+                ) : (
+                    <FaBars style={{ color: "black" }} className="hamburger" onClick={handleClick} />
+                )}
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
                     <li className="nav-item" >
-                        <a href="#inicio" style={{ textDecoration: "none" }}>
+                        <a href="#inicio" style={{ textDecoration: "none" }} onClick={closeMenu}>
                             Inicio
                         </a>
                     </li>
                     <li className="nav-item" >
-                        <a href="#about" style={{ textDecoration: "none" }}>
+                        <a href="#about" style={{ textDecoration: "none" }} onClick={closeMenu}>
                             Acerca de Nosotros
                         </a>
                     </li>
                     <li className="nav-item" >
-                        <a href="#catalog" style={{ textDecoration: "none" }}>
+                        <a href="#catalog" style={{ textDecoration: "none" }} onClick={closeMenu}>
                             Catálogo
                         </a>
                     </li>
                     <li className="nav-item">
-                        <a href="#contact" style={{ textDecoration: "none" }}>
+                        <a href="#contact" style={{ textDecoration: "none" }} onClick={closeMenu}>
                             Contacto
                         </a>
                     </li>
